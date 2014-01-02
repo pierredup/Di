46efcb1ddb96e914e19c189dfdd285f5478b226e
Pierre du Plessis <pierre@pcservice.co.za>
2014-01-02 11:03:34 +0200

    Fixed tests bootstrap to include Di.php directly instead of relying on composer autoloader

--- Test/bootstrap.php
+++ Test/bootstrap.php
@@ -2,7 +2,7 @@
 
 define('FIXTURES', __DIR__ . '/fixtures/');
 
-require_once dirname(__DIR__) . '/vendor/autoload.php';
+require_once dirname(__DIR__) . '/src/Di.php';
 require_once FIXTURES . '/Foo.php';
 require_once FIXTURES . '/Bar.php';
 require_once FIXTURES . '/Baz.php';
